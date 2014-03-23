c3a09913876406eae7c5b7f1bb729b272bf7f822
Holger Maerz <holger.maerz@flagbit.de>
2014-03-23 12:36:10 +0100

    expanding abstract controller for repo

--- module/Nakade/src/Nakade/Abstracts/AbstractController.php
+++ module/Nakade/src/Nakade/Abstracts/AbstractController.php
@@ -2,47 +2,91 @@
 namespace Nakade\Abstracts;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\ServiceManager\FactoryInterface;
 
 /**
  * Extending for having a service getter and setter
  *
  * @author Dr.Holger Maerz <holger@nakade.de>
  */
-class AbstractController extends AbstractActionController {
-   
+class AbstractController extends AbstractActionController
+{
+
     protected $_service;
     protected $_formFactory;
-    
+    protected $_repository;
+
+    /**
+     * @return mixed
+     */
     public function getService()
     {
-       return $this->_service;    
+       return $this->_service;
     }
-    
+
+    /**
+     * @param mixed $service
+     *
+     * @return $this
+     */
     public function setService($service)
     {
         $this->_service = $service;
         return $this;
     }
-    
+
+    /**
+     * @return AbstractFormFactory
+     */
     public function getFormFactory()
     {
-       return $this->_formFactory;    
+       return $this->_formFactory;
     }
-    
+
+    /**
+     * @param AbstractFormFactory $factory
+     *
+     * @return $this
+     */
     public function setFormFactory(AbstractFormFactory $factory)
     {
         $this->_formFactory = $factory;
         return $this;
     }
-    
+
+    /**
+     * @param string $typ
+     *
+     * @return null
+     */
     public function getForm($typ)
     {
-       
-        if(null===$this->_formFactory)
+
+        if (null === $this->_formFactory) {
             return null;
-         
+        }
+
         return $this->_formFactory->getForm($typ);
-        
+
+    }
+
+    /**
+     * @param FactoryInterface $repository
+     *
+     * @return $this
+     */
+    public function setRepository(FactoryInterface $repository)
+    {
+        $this->_repository = $repository;
+        return $this;
+    }
+
+    /**
+     * @return FactoryInterface
+     */
+    public function getRepository()
+    {
+        return $this->_repository;
     }
 }
 
