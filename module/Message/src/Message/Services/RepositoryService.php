d379c4822a22da025fbfc4153ca1b1709fb1c487
Holger Maerz <holger.maerz@flagbit.de>
2014-03-23 12:30:51 +0100

    introducing repository as service in controller

---
+++ module/Message/src/Message/Services/RepositoryService.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Message\Services;
+
+use Message\Mapper\MessageMapper;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use RuntimeException;
+
+
+/**
+ * Creates a mapper factory for doctrine database access.
+ * Use the fabric method for getting the mapper required.
+ */
+class RepositoryService implements FactoryInterface
+{
+
+    protected $_entityManager;
+
+
+    /**
+     * @param ServiceLocatorInterface $services
+     *
+     * @return \Zend\ServiceManager\FactoryInterface
+     *
+     * @throws \RuntimeException
+     */
+    public function createService(ServiceLocatorInterface $services)
+    {
+        //EntityManager for database access by doctrine
+        $this->_entityManager = $services->get('Doctrine\ORM\EntityManager');
+
+        if (null === $this->_entityManager) {
+            throw new RuntimeException(
+                sprintf('Entity manager could not be found in service.')
+            );
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * fabric method for getting the mail needed. expecting the mail name as
+     * string. Throws an exception if provided typ is unknown.
+     * Typ: - 'user'    => users and profile
+     *
+     * @param string $typ
+     *
+     * @return \Nakade\Abstracts\AbstractMapper
+     *
+     * @throws RuntimeException
+     */
+    public function getMapper($typ)
+    {
+
+        switch (strtolower($typ)) {
+
+           case "message":
+               $mapper = new MessageMapper();
+               break;
+
+
+           default:
+               throw new RuntimeException(
+                   sprintf('An unknown mapper type was provided.')
+               );
+        }
+
+        $mapper->setEntityManager($this->_entityManager);
+
+        return $mapper;
+    }
+
+}
