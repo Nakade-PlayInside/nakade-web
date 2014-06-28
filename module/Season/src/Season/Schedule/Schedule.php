0d8d13b463dc0a3ede6bf0ac3059ce56878f855f
Holger Maerz <holger.maerz@flagbit.de>
2014-06-28 21:50:57 +0200

    function for putting last element to seconde position

--- module/Season/src/Season/Schedule/Schedule.php
+++ module/Season/src/Season/Schedule/Schedule.php
@@ -10,12 +10,10 @@ class Schedule {
 
     const BYE = 'FREILOS';
     protected $_startdate;
-    protected $_isSingleGameAMatchday;
     protected $_week=0;
 
-    public function __construct($date, $course=false) {
+    public function __construct($date) {
 
-        $this->_isSingleGameAMatchday=$course;
         $this->_startdate = $date;
     }
 
@@ -28,36 +26,58 @@ class Schedule {
         }
 
         $plan       = array();  // Array für den kompletten Spielplan
-        $nogame     = 0;        // Zähler für Spielnummer
+        $noGame     = 0;        // Zähler für Spielnummer
 
-
-
-        for ($matchday=1; $matchday<count($teams); $matchday++) {
+        for ($round=1; $round<count($teams); $round++) {
 
           //rearrangement of team array
-          array_splice($teams, 1, 1, array(array_pop($teams),$teams[1]));
+            //array_pop -> getLastElement and shorten array by element
+            //array_splice -> remove first element and replace it by last element followed by first element
 
+          //put the last element at first place
+          //array_splice($teams, 1, 1, array(array_pop($teams),$teams[1]));
+/**
+            $teams = array(1,2,3,4,5);
 
-                for ($nopairing=0; $nopairing<(count($teams)/2); $nopairing++) {
+            for ($i=0; $i<2; $i++) {
 
-                    $home =  $teams[$nopairing];
-                    $away =  $teams[(count($teams)-1)-$nopairing];
+                $first = $teams[1];
+                $last = array_pop($teams);
+                $replacement = array($last,$first);
+                array_splice($teams, 1, 1, $replacement);
+//1,5,2,3,4
+ *
+ *
+ * $teams = array(1,2,3,4,5);
+for ($i=0; $i<2; $i++) {
+
+$this->moveLastToFirst($teams);
+}
+ *
+            }*/
+
+            $this->moveLastToFirst($teams);
+
+                for ($noPairing=0; $noPairing<(count($teams)/2); $noPairing++) {
+
+                    $home =  $teams[$noPairing];
+                    $away =  $teams[(count($teams)-1)-$noPairing];
 
                     if ($home == self::BYE || $away == self::BYE ) {
                         continue;
                     }
 
-                    $nogame++;
+                    $noGame++;
 
                     //change home and away
-                    if ( ($nogame%count($teams)==1) && ($nopairing%2==0) ) {
+                    if ( ($noGame%count($teams)==1) && ($noPairing%2==0) ) {
                         $temp = $home;
                         $home = $away;
                         $away = $temp;
                     }
 
-                    $plan[$matchday][$nogame]['away'] = $away;
-                    $plan[$matchday][$nogame]['home'] = $home;
+                    $plan[$round][$noGame]['away'] = $away;
+                    $plan[$round][$noGame]['home'] = $home;
 
                 }
         }
@@ -66,6 +86,17 @@ class Schedule {
         $pairing  = $this->makePairings($plan);
         return $pairing;
 
+    }
+
+    /**
+     * @param array &$players
+     */
+    private function moveLastToFirst(array &$players)
+    {
+        $first = $players[1];
+        $last = array_pop($players); //cut last and pop it
+        $replacement = array($last,$first);
+        array_splice($players, 1, 1, $replacement);
     }
 
     private function makePairings($plan)
@@ -77,11 +108,6 @@ class Schedule {
             $date =  $this->getMatchDate($matchday);
 
             foreach ($matchdaypairing as $game => $pairing) {
-
-                //each game another date
-                if ($this->_isSingleGameAMatchday) {
-                    $date = $this->getMatchDate($game);
-                }
 
                 $data['matchday'] = $matchday;
                 $data['league']   = $pairing['away']->getLeague();
@@ -113,8 +139,4 @@ class Schedule {
 
     }
 
-
-
 }
-
-?>
