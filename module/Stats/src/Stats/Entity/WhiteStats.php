f1279bcf47447e45c1cf7504ab93ce8bf221ce94
Holger Maerz <holger.maerz@flagbit.de>
2014-11-11 09:41:18 +0100

    adding highcharts and first pie chart

---
+++ module/Stats/src/Stats/Entity/WhiteStats.php
@@ -0,0 +1,15 @@
+<?php
+namespace Stats\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class WhiteStats
+ *
+ * @package Stats\Entity
+ */
+class WhiteStats extends AbstractMatchStats
+{
+
+
+}
